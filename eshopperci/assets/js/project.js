d44338a8173c3c7069c28b6398f07af13064f8d8
poojajadhav122 <38005358+poojajadhav122@users.noreply.github.com>
2018-09-07 21:38:02 +0530

    Add files via upload

--- eshopperci/assets/js/project.js
+++ eshopperci/assets/js/project.js
@@ -1,88 +1,169 @@
 $(document).ready(function(){
-    if(localStorage.cartcnt){
-    	$(".cartcnt").html(localStorage.cartcnt)
-    }
-    else{
-    	$(".cartcnt").html(0)
-    }
+
+	if(localStorage.cartcnt){
+
+		$(".cartcnt").html(localStorage.cartcnt)
+	}
+	else{
+
+		$(".cartcnt").html(0);
+	}
+
 	//console.log(curl)
-	$.get(curl+"get_categories",function(data,status){
+
+	$.get(curl+ "get_categories", function(data, status){
+
 		//console.log(status)
 		if(status=="success"){
-			console.log(JSON.parse(data))
+
+			//console.log(JSON.parse(data))
 		}
 	});
 
-	$.get(curl+"get_brands",function(data,status){
-		console.log(data)
+	$.get(curl+ "get_brands", function(data, status){
+
+		
+		//console.log(status)
 		if(status=="success"){
+
+			//console.log(data)
+
 			$(".brands_data").html(data)
-			// onsole.log(JSON.parse(data))
-			// var str=""
-			// $.each(JSON.parse(data),function(key,val){
-			// 	console.log(val)
-			// 	str = str + "<li><a href='#'>"+val.br_name+"</a></li>";
-			// });
-			// //console.log(str)
-			// $(".brands_data").html(str)
+
 		}
 	});
 
+	$.get(curl+ "get_products", function(data, status){
 
-	$.get(curl+"get_products",function(data,status){
 		//console.log(status)
 		if(status=="success"){
-           console.log(data)
-            $(".features_items").html(data)
-		}
 
+			//console.log(data)
+
+			$(".features_items").html(data)
+
+		}
 	});
 
 	$(document).on("click",".br_data",function(aobj){
+
 		aobj.preventDefault();
-		var id = $(this).attr("for")
-		alert(id);
+		var id= $(this).attr("for")
+		//alert(id);
+
 		//alert("brid="+id)
 		$.ajax({
 			type:"post",
 			data:"brid="+id,
-			url:curl+"get_products_brandwise",
+			url: curl+"get_products_brandwise",
 			success:function(response){
-				// alert(response)
-				console.log(response)
+
+				//alert(response);
 				$(".features_items").html(response)
+
 			}
 		})
 	})
+
 	$(document).on("click",".add-to-cart",function(aobj){
+
 		aobj.preventDefault();
-		id = $(this).attr("for")
+		var id= $(this).attr("for");
 		//alert(id);
-		//alert("brid="+id)
+
+		//alert("p_id="+id)
 		$.ajax({
 			type:"post",
 			data:"proid="+id,
-			url:curl+"cart",
+			url: curl+"cart",
 			success:function(response){
-				//alert(response)
-				console.log(response)
+
+				//alert(response);
+				//$(".features_items").html(response)
 				rans = response.split("#")
 				$(".cartcnt").html(rans[0])
 				localStorage.cartcnt = rans[0];
-				alert(rans[1])
-				//$(".features_items").html(response)
-			}
-		});
-	});
+				alert(rans[1]);
 
-	/// check cart//
-	$.get(curl+"get_cart_products",function(data,status){
-		console.log(status)
+			}
+		})
+
+	})
+
+	//cart check
+
+	$.get(curl+ "get_cart_products", function(data, status){
+
+		//console.log(status)
 		if(status=="success"){
-			// console.log(data)
+
+			//console.log(data)
+
 			$(".cart_items").html(data)
+
 		}
 	});
-	/// check cart//
 
-});
+	$(document).on("click",".delete-to-cart",function(obj){
+		if(confirm("want to delete")){
+			curele = $(this);
+			obj.preventDefault();
+			id = $(this).attr("for");
+		}
+
+		//alert(id);
+		$.ajax({
+			type:"post",
+			data:"proid="+id+"&x=10",
+			url: curl+"deletecart",
+			success:function(response){
+
+				//console.log(response);
+
+				rans = response.split("#")
+				$(".cartcnt").html(rans[0])
+				localStorage.cartcnt = rans[0];
+				//alert(rans[1]);
+
+				curele.parent().parent().parent().parent().fadeOut(1000)
+				
+
+			}
+		})
+
+
+	});
+
+	$("#register_form").submit(function(obj){
+		obj.preventDefault();
+		//alert("submitted");
+
+		$.ajax({
+			type:"post",
+			data:$(this).serialize(),
+			url:curl+ "registerAction",
+			success:function(response){
+
+				//console.log(response)
+				$(".err_register").html(response);
+			}
+		})
+	})
+
+	$("#login_form").submit(function(obj){
+		obj.preventDefault();
+		//alert("submitted");
+
+		$.ajax({
+			type:"post",
+			data:$(this).serialize(),
+			url:curl+ "loginAction",
+			success:function(response){
+
+				//console.log(response)
+				$(".err_login").html(response);
+			}
+		})
+	})
+
+})
