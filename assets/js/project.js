d5f274fffd4c6ebbce177bed16270cb17fe04ff9
poojajadhav122 <38005358+poojajadhav122@users.noreply.github.com>
2018-09-13 15:28:18 +0530

    Add files via upload

--- assets/js/project.js
+++ assets/js/project.js
@@ -1,79 +1,201 @@
 $(document).ready(function(){
-	$(".btn_register").click(function(){
-		// alert(1)
-		// console.log( $("#register_form") );
-		record = $("#register_form").serialize();
-		// console.log(record);
+	$.get(curl+"check_users",function(data,status){
+		console.log(data)
+		if(data==0){
+			console.log(localStorage)
 
-		$.post("register-action.php" , record, function(response){
-			// console.log(response);
-			$(".msg").html(response);
+			localStorage.removeItem("username");
+			localStorage.removeItem("userstatus");
+	var link = "<li><a href='login.html'><i class='fa fa-lock'></i>Login</a></li>"
+
+		}
+		else{
+			uname = localStorage.username
+			ustatus = localStorage.userstatus
+	var link ="<li><a href='"+curl+"logout'><i class='fa fa-lock'></i>Logout ("+uname+")</a></li>";
+
+		}
+
+		$("#links").html(link)
+	});
+
+	if(localStorage.cartcnt){
+
+		$(".cartcnt").html(localStorage.cartcnt)
+	}
+	else{
+
+		$(".cartcnt").html(0);
+	}
+
+	//console.log(curl)
+
+	$.get(curl+ "get_categories", function(data, status){
+
+		//console.log(status)
+		if(status=="success"){
+
+			//console.log(JSON.parse(data))
+		}
+	});
+
+	$.get(curl+ "get_brands", function(data, status){
+
+		
+		//console.log(status)
+		if(status=="success"){
+
+			//console.log(data)
+
+			$(".brands_data").html(data)
+
+		}
+	});
+
+	$.get(curl+ "get_products", function(data, status){
+
+		//console.log(status)
+		if(status=="success"){
+
+			//console.log(data)
+
+			$(".features_items").html(data)
+
+		}
+	});
+
+	$(document).on("click",".br_data",function(aobj){
+
+		aobj.preventDefault();
+		var id= $(this).attr("for")
+		//alert(id);
+
+		//alert("brid="+id)
+		$.ajax({
+			type:"post",
+			data:"brid="+id,
+			url: curl+"get_products_brandwise",
+			success:function(response){
+
+				//alert(response);
+				$(".features_items").html(response)
+
+			}
 		})
 	})
 
-	$(".btn_login").click(function(){
-		$.post("login-action.php" , $("#login_form").serialize(), function(response){
-			// console.log(response);
-			if(response=="ok"){
-				window.location.href="index.php";
+	$(document).on("click",".add-to-cart",function(aobj){
+
+		aobj.preventDefault();
+		var id= $(this).attr("for");
+		//alert(id);
+
+		//alert("p_id="+id)
+		$.ajax({
+			type:"post",
+			data:"proid="+id,
+			url: curl+"cart",
+			success:function(response){
+
+				//alert(response);
+				//$(".features_items").html(response)
+				rans = response.split("#")
+				$(".cartcnt").html(rans[0])
+				localStorage.cartcnt = rans[0];
+				alert(rans[1]);
+
 			}
-			else{
-				$(".msg1").html(response);
+		})
+
+	})
+
+	//cart check
+
+	$.get(curl+ "get_cart_products", function(data, status){
+
+		//console.log(status)
+		if(status=="success"){
+
+			//console.log(data)
+
+			$(".cart_items").html(data)
+
+		}
+	});
+
+	$(document).on("click",".delete-to-cart",function(obj){
+		if(confirm("want to delete")){
+			curele = $(this);
+			obj.preventDefault();
+			id = $(this).attr("for");
+		}
+
+		//alert(id);
+		$.ajax({
+			type:"post",
+			data:"proid="+id+"&x=10",
+			url: curl+"deletecart",
+			success:function(response){
+
+				//console.log(response);
+
+				rans = response.split("#")
+				$(".cartcnt").html(rans[0])
+				localStorage.cartcnt = rans[0];
+				//alert(rans[1]);
+
+				curele.parent().parent().parent().parent().fadeOut(1000)
+				
+
+			}
+		})
+
+
+	});
+
+	$("#register_form").submit(function(obj){
+		obj.preventDefault();
+		//alert("submitted");
+
+		$.ajax({
+			type:"post",
+			data:$(this).serialize(),
+			url:curl+ "registerAction",
+			success:function(response){
+
+				//console.log(response)
+				$(".err_register").html(response);
 			}
 		})
 	})
 
-	/*************************************/
-	$("#forgot2_form,#forgot3_form").hide();
-	$(".btn_forgot1").click(function(){
-		$.post("forgot-action1.php" , $("#forgot1_form").serialize() ,function(response){
-			if(response == "ok"){
-				$(".msg").empty();
-				$("#forgot1_form")[0].reset();
-				$("#forgot2_form,#forgot1_form").slideToggle();
+	$("#login_form").submit(function(obj){
+		obj.preventDefault();
+		//alert("submitted");
+
+		$.ajax({
+			type:"post",
+			data:$(this).serialize(),
+			url:curl+ "loginAction",
+			success:function(response){
+
+				//console.log(response)
+				//ok#1
+				ans = response.split("#")
+				if(ans[0]== "ok"){
+					// consloe.log(ans[2])
+					// consloe.log(ans[1])
+					// consloe.log(ans[0])
+					localStorage.setItem("userstatus",ans[1])
+					localStorage.setItem("username",ans[2])
+					window.location.href="index.html"
+				}
+				else{
+					$(".err_login").html(response);
 			}
-			else{
-				$(".msg").html(response);
+				
 			}
-		})
+		});
 	});
 
-	$(".btn_forgot2").click(function(){
-		$.post("forgot-action2.php" , $("#forgot2_form").serialize() ,function(response){
-			if(response == "ok"){
-				$(".msg").empty();
-				$("#forgot2_form")[0].reset();
-				$("#forgot2_form,#forgot3_form").slideToggle();
-			}
-			else{
-				$(".msg").html(response);
-			}
-		})
-	});
-
-	$(".btn_forgot3").click(function(){
-		$.post("forgot-action3.php" , $("#forgot3_form").serialize() ,function(response){
-			if(response == "ok"){
-				$("#forgot3_form")[0].reset();
-				$(".msg").html("password updated");
-			}
-			else{
-				$(".msg").html(response);
-			}
-		})
-	});
-	/*************************************/
-	$(".btn_pass").click(function(){
-		$.post("password-action.php",$("#password_form").serialize(),function(res){
-			// console.log(res);
-			if(res=="ok"){
-				$(".msg").html("password updated");
-				$("#password_form")[0].reset();
-			}
-			else{
-				$(".msg").html(res);
-			}
-		})
-	})
-	/*************************************/
 });
